21c10edd4d393d4fb554651d28c3bb0987d564e7
sukritiverma1996 <verma.sukriti.1996@gmail.com>
2016-08-07 12:43:33 +0530

    WORKING - CHANGES MADE

--- fb_chatbot/views.py
+++ fb_chatbot/views.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import json, requests, random, re
 from pprint import pprint
 
@@ -11,15 +12,39 @@ from django.utils.decorators import method_decorator
 PAGE_ACCESS_TOKEN='EAATD9Q2WbQgBAMyb3chgCdZAokOx0Wn4iV3mgPQoN4uLF7CwYJg2HfNHNUyhvpQKNf0r6kQClgL2NRnPPAxl5db1n6ZBeZA1FyZBcks5zGUDaVZBlDQsasTZBKV1it9sOB3t2xxbtODr3mPNuAIAdsmltk3WdAg87Y1TC0AK5H5AZDZD'
 VERIFY_TOKEN = '8447789934'
 
+quotes_arr = [["Life isn’t about getting and having, it’s about giving and being.", "Kevin Kruse"],
+["Whatever the mind of man can conceive and believe, it can achieve.", "Napoleon Hill"],
+["Strive not to be a success, but rather to be of value.", "Albert Einstein"],
+["Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.", "Robert Frost"],
+["I attribute my success to this: I never gave or took any excuse.", "Florence Nightingale"],
+["You miss 100% of the shots you don’t take.", "Wayne Gretzky"],
+["I’ve missed more than 9000 shots in my career. I’ve lost almost 300 games. 26 times I’ve been trusted to take the game winning shot and missed. I’ve failed over and over and over again in my life. And that is why I succeed.", "Michael Jordan"],
+["The most difficult thing is the decision to act, the rest is merely tenacity.", "Amelia Earhart"],
+["Every strike brings me closer to the next home run.", "Babe Ruth"],
+["Definiteness of purpose is the starting point of all achievement.", "W. Clement Stone"],
+["We must balance conspicuous consumption with conscious capitalism.", "Kevin Kruse"],
+["Life is what happens to you while you’re busy making other plans.", "John Lennon"],
+["We become what we think about.", "Earl Nightingale"],
+["14.Twenty years from now you will be more disappointed by the things that you didn’t do than by the ones you did do, so throw off the bowlines, sail away from safe harbor, catch the trade winds in your sails.  Explore, Dream, Discover.", "Mark Twain"],
+["15.Life is 10% what happens to me and 90% of how I react to it.", "Charles Swindoll"],
+["The most common way people give up their power is by thinking they don’t have any.", "Alice Walker"],
+["The mind is everything. What you think you become.", "Buddha"],
+["The best time to plant a tree was 20 years ago. The second best time is now.", "Chinese Proverb"],
+["An unexamined life is not worth living.", "Socrates"],
+["Eighty percent of success is showing up.", "Woody Allen"],
+["Your time is limited, so don’t waste it living someone else’s life.", "Steve Jobs"],
+["Winning isn’t everything, but wanting to win is.", "Vince Lombardi"],
+["I am not a product of my circumstances. I am a product of my decisions.", "Stephen Covey"],
+["Every child is an artist.  The problem is how to remain an artist once he grows up.", "Pablo Picasso"]]
 
 def post_facebook_message(fbid, recevied_message):
-    reply_text = recevied_message + ':)'
+    reply_text = return_random_quote()
 
     try:
         user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid 
         user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN} 
         user_details = requests.get(user_details_url, user_details_params).json() 
-        joke_text = 'BOT: '+user_details['first_name']+'..! ' + reply_text
+        joke_text = 'BOT: '+user_details['first_name']+', ' + reply_text
     except:
         joke_text = 'BOT: ' + reply_text
 
@@ -65,8 +90,13 @@ class MyQuoteBotView(generic.View):
 
 
 def index(request):
-    print test()
+    print return_random_quote()
     return HttpResponse("Hello World")
 
 def test():
     post_facebook_message('abhishek.sukumar.1','test message')
+
+
+def return_random_quote():
+    random.shuffle(quotes_arr)
+    return quotes_arr[0][0]

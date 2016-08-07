8b23770cac75cc7bbe07b452f9061ed96fde5c5b
sukritiverma1996 <verma.sukriti.1996@gmail.com>
2016-08-07 10:48:00 +0530

    changes 2

--- fb_chatbot/urls.py
+++ fb_chatbot/urls.py
@@ -1,11 +1,11 @@
-from django.conf.urls import patterns, url
-from fb_chatbot import views
-from fb_chatbot.views import MyQuoteBotView
 
 # r'^$' is a regex string
 # It deals with empty URLs
+from django.conf.urls import patterns,url
+from fb_chatbot import views
+from .views import MyQuoteBotView
+
+
 urlpatterns = patterns('',
-    url(r'^$', views.hello_world, name = 'hello_world'),
-    url(r'^hi/$', views.hello, name = 'hello'),
-    url(r'^hi/hello$', views.hello, name = 'hello'),
-    url(r'^facebook_auth/$', MyQuoteBotView.as_view()),)
+    url(r'^$', views.index,name = 'index'),
+url(r'^facebook_auth/?$', MyQuoteBotView.as_view()))
--- fb_chatbot/views.py
+++ fb_chatbot/views.py
@@ -7,51 +7,37 @@ from django.http.response import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.utils.decorators import method_decorator
 
-quotes_string = '''
-There is no charge for awesomeness... or attractiveness.;Kung Fu Panda;http://www.google.co.in/search?ie=UTF-8&q=kung+fu+panda+awesomeness
-Any fool can write code that a computer can understand. Good programmers write code that humans can understand.;Martin Fowle;http://thc.org/root/phun/unmaintain.html
-He was so deadly, in fact, that his enemies would go blind from over-exposure to pure awesomeness!;Kung Fu Panda;
-One often meets his destiny on the road he takes to avoid it.;Kung Fu Panda;
-The world is moved along, not only by the mighty shoves of its heroes, but also by the aggregate of the tiny pushes of each honest worker.;Helen Keller;
-'''
-
-quotes_arr = quotes_string.split('\n')
 
 PAGE_ACCESS_TOKEN ='EAATD9Q2WbQgBAMyb3chgCdZAokOx0Wn4iV3mgPQoN4uLF7CwYJg2HfNHNUyhvpQKNf0r6kQClgL2NRnPPAxl5db1n6ZBeZA1FyZBcks5zGUDaVZBlDQsasTZBKV1it9sOB3t2xxbtODr3mPNuAIAdsmltk3WdAg87Y1TC0AK5H5AZDZD'
+VERIFY_TOKEN = '8447789934'
+
 
 def post_facebook_message(fbid, recevied_message):
-    # Remove all punctuations, lower case the text and split it based on space
-    #clean_message = re.sub(r"[^a-zA-Z0-9\s]",' ',recevied_message)
+    reply_text = recevied_message + ':)'
 
-    reply_text = ''
+    try:
+        user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid 
+        user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN} 
+        user_details = requests.get(user_details_url, user_details_params).json() 
+        joke_text = 'Yo '+user_details['first_name']+'..! ' + reply_text
+    except:
+        joke_text = 'Yo ' + reply_text
+
     
-
-    if not reply_text:
-        reply_text = recevied_message
-
-    user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid 
-    user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN} 
-    user_details = requests.get(user_details_url, user_details_params).json() 
-    joke_text = 'Yo '+user_details['first_name']+'..! ' + reply_text
                    
     post_message_url = 'https://graph.facebook.com/v2.6/me/messages?access_token=%s'%PAGE_ACCESS_TOKEN
     response_msg = json.dumps({"recipient":{"id":fbid}, "message":{"text":joke_text}})
     status = requests.post(post_message_url, headers={"Content-Type": "application/json"},data=response_msg)
     pprint(status.json())
 
-# Create your views here.
 
 class MyQuoteBotView(generic.View):
     def get(self, request, *args, **kwargs):
-        try:
-            # that number is your webhook token which youll set up in a bit
-            if self.request.GET['hub.verify_token'] == '8447789934':
-                return HttpResponse(self.request.GET['hub.challenge'])
-            else:
-                return HttpResponse('Error, invalid token')
-        except:
+        if self.request.GET['hub.verify_token'] == VERIFY_TOKEN:
+            return HttpResponse(self.request.GET['hub.challenge'])
+        else:
             return HttpResponse('Error, invalid token')
-
+        
     @method_decorator(csrf_exempt)
     def dispatch(self, request, *args, **kwargs):
         return generic.View.dispatch(self, request, *args, **kwargs)
@@ -72,14 +58,15 @@ class MyQuoteBotView(generic.View):
                     # Assuming the sender only sends text. Non-text messages like stickers, audio, pictures
                     # are sent as attachments and must be handled accordingly. 
                     post_facebook_message(message['sender']['id'], message['message']['text'])    
+                    
+
         return HttpResponse()    
 
 
 
-# Create your views here.
+def index(request):
+    print test()
+    return HttpResponse("Hello World")
 
-def hello_world(request):
-    return HttpResponse("<b><i>Hello World</i></b>")
-
-def hello(request):
-    return HttpResponse("Hey Hey!")
+def test():
+post_facebook_message('abhishek.sukumar.1','test message')

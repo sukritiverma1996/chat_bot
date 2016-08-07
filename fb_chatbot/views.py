d40999ff91f610ef670f6ae5ac6043da0b1f71a0
sukritiverma1996 <verma.sukriti.1996@gmail.com>
2016-08-07 11:31:49 +0530

    WORKING - CHANGES MADE

--- fb_chatbot/views.py
+++ fb_chatbot/views.py
@@ -19,9 +19,9 @@ def post_facebook_message(fbid, recevied_message):
         user_details_url = "https://graph.facebook.com/v2.6/%s"%fbid 
         user_details_params = {'fields':'first_name,last_name,profile_pic', 'access_token':PAGE_ACCESS_TOKEN} 
         user_details = requests.get(user_details_url, user_details_params).json() 
-        joke_text = 'Yo '+user_details['first_name']+'..! ' + reply_text
+        joke_text = 'BOT: '+user_details['first_name']+'..! ' + reply_text
     except:
-        joke_text = 'Yo ' + reply_text
+        joke_text = 'BOT: ' + reply_text
 
     
                    
